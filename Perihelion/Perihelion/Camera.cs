99a96a0b2d1acb5619d066f71c8c4bd8b36b1732
Thomas Nilsen <thnilsen@broadpark.no>
2012-10-17 16:22:09 +0200

    test test test

--- Perihelion/Perihelion/Camera.cs
+++ Perihelion/Perihelion/Camera.cs
@@ -22,7 +22,6 @@ namespace Perihelion
         public Camera(Viewport view)
         {
             this.view = view;
-            cameraOffset = new Vector2(view.Width/2, view.Height/2);
         }
 
         public Matrix Transform
@@ -49,16 +48,11 @@ namespace Perihelion
 
         public void update(Vector2 objectToFollow)
         {
-            this.center = objectToFollow - cameraOffset;
-//             this.center = new Vector2(picture.spritePosition.X + (picture.spriteRectangle.Width / 2 - 400)
-//             , picture.spritePosition.Y + (picture.spriteRectangle.Height / 2 - 250));
-
-            //this.transform = Matrix.CreateScale(new Vector3(this.zoom, this.zoom, 0)) *
-            //Matrix.CreateTranslation(new Vector3(-this.center.X, -this.center.Y, 0));
+            this.center = objectToFollow<
 
             this.transform = Matrix.CreateTranslation(new Vector3(-this.center.X, -this.center.Y, 0)) *
-                Matrix.CreateScale(new Vector3(this.zoom, this.zoom, 0)); //*
-                //Matrix.CreateTranslation(new Vector3(this.view.Width / 2, this.view.Height / 2, 0));
+                Matrix.CreateScale(new Vector3(this.zoom, this.zoom, 0))*
+                Matrix.CreateTranslation(new Vector3(this.view.Width / 2, this.view.Height / 2, 0));
         }
 
         public void getZoomInput()
