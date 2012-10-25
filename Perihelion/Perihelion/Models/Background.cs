411e4b60c1ab14c701a53d48ce50cbac6030aab5
Inge Dalby <inge.dalby@gmail.com>
2012-10-25 16:48:47 +0200

    Rewrote Background class - Parallaxing is now based on velocity, not position. - Only one constructor. Adding support for objects etc SOON - Changed loading of background textures. they are now stored in a Texture2D array.

--- Perihelion/Perihelion/Models/Background.cs
+++ Perihelion/Perihelion/Models/Background.cs
@@ -10,67 +10,75 @@ namespace Perihelion.Models
 	class Background
 	{
 		// XNA types
-		private Texture2D texture;
-		private Texture2D[] textures;           // Used only with the second constructor
-		private Vector2[] position;             // Position of the first tile
+		private Texture2D[] textures;           // Stores an array of textures.
 		private Vector2 prevPlayerPos;
-
-		// Background specific variables
+        
+        // Background specific variables
 		private int offset;
-		private int numOfTiles;                 // set in the createBackground method. Represents both x and y
+        private int tilesWidth;
+        private int tilesHeight;
+		private int numOfTiles;
 		private float increment;
         private bool parallaxingObject;
+        private Random random = new Random();
+
+        // fRectangle struct
+        public struct fRectangle {
+            public Vector2 position;                 // An array of positions for all the tiles in the fRectangle.
+            public int width;
+            public int height;
+
+            public fRectangle(Vector2 newPos, int newWidth, int newHeight)
+            {
+                position = newPos;
+                width = newWidth;
+                height = newHeight;
+            }
+        };
+        private fRectangle backgroundRect;
+
+
 
 		/**********************************/
 		/* Constructors                   */
 		/**********************************/
-		public Background(Texture2D texture, Vector2 position, Vector2 prevPlayerPos, Rectangle levelBounds, float increment)
+		public Background(Texture2D[] textures, Vector2 position, Vector2 velocity, Rectangle levelBounds, float increment)
 		{
-			Texture = texture;
-			createBackground(levelBounds, position);
+            Offset = textures[0].Width;
+            setRectangle(levelBounds);
+            setTextures(textures);
+			//createBackground(levelBounds, position);
 			PrevPlayerPos = prevPlayerPos;
 			Increment = increment;
-			Offset = Texture.Width;
             ParallaxingObject = false;
-		}
-
-		// The following constructor should be used when wanting to parallax big
-		//  objects (planets etc), and not entire backgrounds. The Texture2D array should
-		//  be the correct number of tiles required to display the object correctly
-		//  (for example, a planet that consists of 9 128x128 textures should have
-		//  all it's textures passed in this array, in the CORRECT ORDER)
-		public Background(Texture2D[] textures, Vector2 position, Vector2 prevPlayerPos, int tilesWidth, int tilesHeight, float increment)
-		{
-            createBackground(position, textures, tilesHeight, tilesWidth);
-            ParallaxingObject = true;
 		}
 
 		/**********************************/
 		/* Accessors                      */
 		/**********************************/
-		public Texture2D Texture
-		{
-			get { return this.texture; }
-			set { this.texture = value; }
-		}
-
         public Texture2D[] Textures
         {
             get { return this.textures; }
             set { this.textures = value; }
         }
 
-		public Vector2[] Position
-		{
-			get { return this.position; }
-			set { this.position = value; }
-		}
-
 		public Vector2 PrevPlayerPos
 		{
 			get { return this.prevPlayerPos; }
 			set { this.prevPlayerPos = value; }
 		}
+
+        public int TilesHeight
+        {
+            get { return this.tilesHeight; }
+            set { this.tilesHeight = value; }
+        }
+
+        public int TilesWidth
+        {
+            get { return this.tilesWidth; }
+            set { this.tilesWidth = value; }
+        }
 
 		public int Offset
 		{
@@ -89,75 +97,37 @@ namespace Perihelion.Models
             get { return this.parallaxingObject; }
             set { this.parallaxingObject = value; }
         }
+
 		/**********************************/
 		/* Methods                        */
 		/**********************************/
-		private void createBackground(Rectangle levelBounds, Vector2 position)
+		private void updatePosition(Vector2 velocity)
 		{
-			numOfTiles = (levelBounds.Width + (offset * 2)) / Texture.Width;        // numOfTiles is actually number of tiles across one axis
-			Position = new Vector2[(numOfTiles * numOfTiles)];
-			
-			for (int y = 0; y < numOfTiles; y++)
-			{
-				for (int x = 0; x < numOfTiles; x++)
-				{
-					Position[(y * numOfTiles) + x] = new Vector2(position.X + (x * Texture.Width), position.Y + (y * Texture.Height));
-				}
-			}
+            backgroundRect.position.X += velocity.X * Increment;
+            backgroundRect.position.Y -= velocity.Y * Increment;
 		}
 
-        // Used in the object constructor
-        private void createBackground(Vector2 position, Texture2D[] textures, int tilesHeight, int tilesWidth)
+        // Sets textures correctly.
+        private void setTextures(Texture2D[] textures)
         {
-            Position = new Vector2[textures.Length];
+            TilesWidth = backgroundRect.width / textures[0].Width;
+            TilesHeight = backgroundRect.height / textures[0].Height;
 
-            // assigns textures correctly
-            for (int i = 0; i < textures.Length; i++)
-            {
-                Textures[i] = textures[i];
-            }
+            Textures = new Texture2D[TilesWidth * TilesHeight];
+            int randomNumber;
 
-            // Positions textures accordingly
-            for (int y = 0; y < tilesHeight; y++)
+            for (int i = 0; i < Textures.Length; i++) 
             {
-                for (int x = 0; x < tilesWidth; x++)
-                {
-                    int j = (y * tilesHeight) + x;
-                    Position[j] = new Vector2(position.X + (x * Textures[j].Width), position.Y + (y * Textures[j].Height));
-                }
+                randomNumber = random.Next(textures.Length);
+                Textures[i] = textures[randomNumber];
             }
+                
         }
 
-		private void updatePosition(Vector2 playerPos)
-		{
-			Vector2 tempPosition = Position[0];
-
-			if (playerPos.X != prevPlayerPos.X)
-			{
-				if (playerPos.X > prevPlayerPos.X)
-					tempPosition.X -= Increment;
-				if (playerPos.X < prevPlayerPos.X)
-					tempPosition.X += Increment;
-			}
-
-			if (playerPos.Y != prevPlayerPos.Y)
-			{
-				if (playerPos.Y > prevPlayerPos.Y)
-					tempPosition.Y -= Increment;
-				if (playerPos.Y < prevPlayerPos.Y)
-					tempPosition.Y += Increment;
-			}
-
-			PrevPlayerPos = playerPos;
-
-			for (int y = 0; y < numOfTiles; y++)
-			{
-				for (int x = 0; x < numOfTiles; x++)
-				{
-					Position[(y * numOfTiles) + x] = new Vector2(tempPosition.X + (x * Texture.Width), tempPosition.Y + (y * Texture.Height));
-				}
-			}
-		}
+        private void setRectangle(Rectangle levelBounds)
+        {
+            backgroundRect = new fRectangle(new Vector2(levelBounds.X - Offset, levelBounds.Y - Offset), levelBounds.Width + (Offset * 2), levelBounds.Height + (Offset* 2));
+        }
 		/**********************************/
 		/* XNA Methods                    */
 		/**********************************/
@@ -168,16 +138,18 @@ namespace Perihelion.Models
 
 		public void Draw(SpriteBatch spriteBatch)
 		{
-            // Draws the background correctly
-            if (ParallaxingObject)
+            Vector2 tempPosition;
+            float textureHeight = Textures[0].Height;
+            float textureWidth = Textures[0].Width;
+
+            for (int y = 0; y < TilesHeight; y++)
             {
-                for (int i = 0; i < Textures.Length; i++)
-                    spriteBatch.Draw(Textures[i], position[i], Color.White);
-            }
-            else
-            {
-                for (int i = 0; i < Position.Length; i++)
-                    spriteBatch.Draw(Texture, Position[i], Color.White);
+                for (int x = 0; x < TilesWidth; x++)
+                {
+                    tempPosition = new Vector2(backgroundRect.position.X + (textureWidth * x), backgroundRect.position.Y + (textureHeight * y));
+
+                    spriteBatch.Draw(Textures[x + (y * TilesWidth)], tempPosition, Color.White);
+                }
             }
         }
 	}
